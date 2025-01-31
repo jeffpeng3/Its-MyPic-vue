4c3ead43865772f590adde950b2c545e72759372
jeff <jeffpeng2012@gmail.com>
2025-02-01 00:48:08 +0800

    feat: fix copy button
    now copy button work
    but unstable

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,44 +1,38 @@
 <template>
-  <v-card
-    color="surface-variant"
-    rounded="lg"
-    variant="tonal"
-    @click="showDialog = true"
-  >
-    <v-img
-      :src="imgUrl"
-      cover
-      width="280px"
-    />
+  <v-card color="surface-variant" rounded="lg" variant="tonal" @click="showDialog = true">
+    <v-img :src="imgUrl" cover width="280px" />
     <v-card-text class="card-text text-center justify-center">
       {{ text }}
     </v-card-text>
   </v-card>
 
-  <v-dialog
-    v-model="showDialog"
-    max-width="600px"
-    :style="{ maxHeight: '90vh' }"
-  >
+  <v-dialog v-model="showDialog" max-width="600px" :style="{ maxHeight: '90vh' }">
     <v-card>
-      <v-img
-        :src="imgUrl"
-        :style="{ maxHeight: '70vh', objectFit: 'contain' }"
-      />
+      <v-img :src="imgUrl" :style="{ maxHeight: '70vh', objectFit: 'contain' }" />
       <v-card-actions>
         <v-btn @click="downloadImage">
           下載
         </v-btn>
-        <!-- <v-btn @click="copyImage">複製</v-btn> -->
+        <v-btn @click="copyImage">複製</v-btn>
         <v-card-text>{{ text }}</v-card-text>
       </v-card-actions>
     </v-card>
   </v-dialog>
+
+  <v-snackbar v-model="copySucess" :timeout=2000 class="text-center" rounded="pill">
+    <div class="text-h6 mx-auto font-weight-bold text-center text-truncate">
+      複製成功
+    </div>
+  </v-snackbar>
+  <v-snackbar v-model="copyFailed" :timeout=2000 class="text-center" rounded="pill">
+    <div class="text-h6 mx-auto font-weight-bold text-center text-truncate">
+      複製失敗
+    </div>
+  </v-snackbar>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-// import type { Slot } from 'vue';
 
 const props = defineProps({
   episode: {
@@ -58,6 +52,8 @@ const props = defineProps({
 const baseUrl = 'https://mygodata.0m0.uk/images/';
 const imgUrl = computed(() => `${baseUrl}${props.episode}_${props.frame_start}.jpg`);
 const showDialog = ref(false);
+const copySucess = ref(false);
+const copyFailed = ref(false);
 
 async function downloadImage() {
   const response = await fetch(imgUrl.value);
@@ -73,17 +69,35 @@ async function downloadImage() {
   URL.revokeObjectURL(url);
 }
 
-// const copyImage = async () => {
-//   try {
-//     const response = await fetch(imgUrl.value);
-//     const blob = await response.blob();
-//     const item = new ClipboardItem({ [`web ${blob.type}`]: blob });
-//     await navigator.clipboard.write([item]);
-//     alert('Image copied to clipboard');
-//   } catch (error) {
-//     console.error('Failed to copy image: ', error);
-//   }
-// };
+const copyImage = async () => {
+  try {
+    let img = new Image();
+    img.src = imgUrl.value;
+    img.crossOrigin = 'Anonymous';
+    img.style.display = 'none';
+    await new Promise((resolve) => {
+      img.onload = resolve;
+    });
+
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d') ?? new CanvasRenderingContext2D();
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+    const blobUrl = canvas.toDataURL("image/png");
+
+    const data = await fetch(blobUrl);
+    const blob = await data.blob();
+    await navigator.clipboard.write([
+      new window.ClipboardItem({
+        [blob.type]: blob
+      })
+    ]);
+    copySucess.value = true;
+  } catch (e) {
+    copyFailed.value = true;
+  }
+};
 </script>
 
 <style scoped>
