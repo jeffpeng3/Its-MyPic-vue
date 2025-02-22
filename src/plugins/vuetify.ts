b05d233005512675161e34387c887ccddc4ccc9a
jeff <jeffpeng2012@gmail.com>
2025-02-23 01:56:34 +0800

    style: fmt

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,50 +5,50 @@
  */
 
 // Styles
-import 'vuetify/styles'
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-import 'vuetify/styles'
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
-import { md3 } from 'vuetify/blueprints'
+import "vuetify/styles";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import { md3 } from "vuetify/blueprints";
 
 export default createVuetify({
   blueprint: md3,
   icons: {
-    defaultSet: 'mdi',
+    defaultSet: "mdi",
     aliases,
     sets: {
       mdi,
     },
   },
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: "dark",
     themes: {
       light: {
         colors: {
-          primary: '#1976D2',
-          secondary: '#424242',
-          accent: '#82B1FF',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FFC107',
+          primary: "#1976D2",
+          secondary: "#424242",
+          accent: "#82B1FF",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FFC107",
         },
       },
       dark: {
         colors: {
-          primary: '#2196F3',
-          secondary: '#424242',
-          accent: '#FF4081',
-          error: '#FF5252',
-          info: '#2196F3',
-          success: '#4CAF50',
-          warning: '#FFC107',
+          primary: "#2196F3",
+          secondary: "#424242",
+          accent: "#FF4081",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FFC107",
         },
       },
     },
   },
-})
+});
